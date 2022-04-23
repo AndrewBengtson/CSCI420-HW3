9d00fd3b29c7e48e821d30c6c5c469a84500bcb4
agbengtson <agbengtson@email.wm.edu>
2022-04-23 16:28:58 -0400

    baseline implementation of model architecture

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ define moduals of model
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.nn as nn
-
+import torch
 
 class CNNModel(nn.Module):
 	"""docstring for ClassName"""
@@ -17,21 +17,25 @@ class CNNModel(nn.Module):
 		##-----------------------------------------------------------
 		
 		## define CNN layers below
-		self.conv = nn.sequential( 	# nn.Conv2d(in_channels,...),
-									# activation fun,
-									# dropout,
-									# nn.Conv2d(in_channels,...),
-									# activation fun,
-									# dropout,
-									## continue like above,
-									## **define pooling (bonus)**,
+		#sequential means it will automatically feed
+		self.conv = nn.sequential( 	nn.Conv2d(1,10,2,2),
+									nn.ReLu(),
+									nn.Dropout(p=0.2),
+									nn.Conv2d(10,8,2,2),
+									nn.ReLu(),
+									nn.Dropout(p=0.2),
+									nn.Conv2d(8,6,2,2),
+									nn.ReLu(),
+									nn.Dropout(p=0.2),
+									nn.Conv2d(6,4,2,2),
+
 								)
 
 		##------------------------------------------------
 		## write code to define fully connected layer below
 		##------------------------------------------------
-		in_size = 
-		out_size = 
+		in_size = 4*28*28
+		out_size = 10 #this is because we have 10 different categories we are attempting to predict
 		self.fc = nn.Linear(in_size, out_size)
 		
 
@@ -41,7 +45,7 @@ class CNNModel(nn.Module):
 		##---------------------------------------------------------
 		## write code to feed input features to the CNN models defined above
 		##---------------------------------------------------------
-		x_out = 
+		x_out = self.conv(x)
 
 		## write flatten tensor code below (it is done)
 		x = torch.flatten(x_out,1) # x_out is output of last layer
@@ -50,12 +54,13 @@ class CNNModel(nn.Module):
 		## ---------------------------------------------------
 		## write fully connected layer (Linear layer) below
 		## ---------------------------------------------------
-		result =   # predict y
+		result =   self.fc(x)
 		
 		
 		return result
         
 		
-		
+def calc_output_size(Width,Kernel,Padding,Stride):
+	return ((Width-Kernel+(2*Padding))/Stride)+1
 	
 		
